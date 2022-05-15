5ee38df262470ce0c14566c3f46e084c064d6033
Sentientplatypus <trexxxxxxxxxxy@gmail.com>
2022-05-14 21:54:30 -0400

    Moved files into here

---
+++ car.js
@@ -0,0 +1,121 @@
+class Car
+{
+    constructor(x,y,width,height)
+    {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.controls = new Controls();
+
+        this.speed = 0;
+        this.maxSpeed = 3;
+        this.friction = 0.05;
+        this.angle = 0;
+
+        this.acceleration = 0.2;
+        this.sensor = new Sensor(this);
+    }
+
+    update(roadBorders)
+    {
+        this.#move();
+        this.sensor.update(roadBorders);
+    }
+
+    #createPolygon()
+    {
+        const points = [];
+        const rad = Math.hypot(this.width, this.height)/2;
+        const alpha = Math.atan2(this.width, this.height);
+        points.push({
+            x:this.x - Math.sin(this.angle - alpha) * rad,
+            y:this.y - Math.cos(this.angle - alpha) * rad
+        })
+        points.push({
+            x:this.x - Math.sin(this.angle + alpha) * rad,
+            y:this.y - Math.cos(this.angle + alpha) * rad
+        })
+        points.push({
+            x:this.x - Math.sin(Math.PI + this.angle - alpha) * rad,
+            y:this.y - Math.cos(Math.PI + this.angle - alpha) * rad
+        })
+        points.push({
+            x:this.x - Math.sin(Math.PI + this.angle + alpha) * rad,
+            y:this.y - Math.cos(Math.PI + this.angle + alpha) * rad
+        })
+        
+    }
+
+
+    #move()
+    {
+        if (this.controls.forward)
+        {
+            this.speed += this.acceleration;
+        }
+        if (this.controls.reverse)
+        {
+            this.speed -= this.acceleration
+        }
+
+        if (this.speed > this.maxSpeed)
+        {
+            this.speed = this.maxSpeed;
+        }
+
+        if (this.speed < -this.maxSpeed/2)
+        {
+            this.speed = -this.maxSpeed/2;
+        }
+
+        if (this.speed > 0)
+        {
+            this.speed -= this.friction;
+        }
+
+        if (this.speed < 0)
+        {
+            this.speed += this.friction;
+        }
+
+        if (Math.abs(this.speed) < this.friction)
+        {
+            this.speed = 0;
+        }
+
+        if (this.speed != 0 )
+        {
+            const flip = this.speed > 0? 1 : -1;
+            if (this.controls.left)
+            {
+                this.angle += 0.03 * flip;
+            }
+            if (this.controls.right)
+            {
+                this.angle -= 0.03 * flip;
+            }
+        }
+
+        this.x -= Math.sin(this.angle) * this.speed;
+        this.y -= Math.cos(this.angle) * this.speed;
+
+    }
+
+    draw(ctx)
+    {
+        ctx.save()
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+        ctx.beginPath();
+        ctx.rect(
+            -this.width/2,
+            -this.height/2,
+            this.width,
+            this.height
+        );
+        ctx.fill();
+        ctx.restore();
+        this.sensor.draw(ctx);
+    }
+}
